1a766eb586f557b1f5f635803fed5809c666f73c
kamstand <kamrul.hassan@stonybrook.edu>
2025-04-04 14:53:42 -0400

    added enums into pcap

--- pcap.d
+++ pcap.d
@@ -7,6 +7,17 @@ import system;
 import std.range;
 import std.algorithm;
 
+enum HEADER_LENGTH = 24; // header is 24 octets
+enum MAJOR_MINOR_LENGTH = 2;
+enum HEADER_CHUNKS // every 4 bytes represent a portion of the header
+{
+  MAGIC = 0,
+  MAJOR_MINOR = 1,
+  SNAP_LEN = 4,
+  LINK_TYEP = 5
+}
+
+
 /****
 
                            1                   2                   3
@@ -65,24 +76,21 @@ struct FileHeader
 
 FileHeader getHeader(ref ubyte[] content, in ENDIAN e)
 {
-  FileHeader h;
-  ubyte[] fileHeader = content[0 .. 24];
-  auto c = chunks(fileHeader, 4);
-  foreach (ubyte[] key; c)
-  {
-    writeln(key);
-  }
+  FileHeader header;
+  ubyte[] fileHeader = content[0 .. HEADER_LENGTH];
+  auto c = chunks(fileHeader, MAGIC_HEADER);
+  
   uint32_t snapLen = read_u32(c[4], e);
   ubyte [] major_minor = c[1];
   writeln(major_minor.length);
-  uint16_t major = read_u16(major_minor[0 .. 2], e);
-  uint16_t minor = read_u16(major_minor[2 .. major_minor.length], e);
+  uint16_t major = read_u16(major_minor[0 .. MAJOR_MINOR_LENGTH], e);
+  uint16_t minor = read_u16(major_minor[MAJOR_MINOR_LENGTH .. $], e);
   uint32_t magic = read_u32(c[0], e);
   uint16_t linkType = read_u16(c[5], e);
-  h.magicNumber = magic;
-  h.linkType = linkType;
-  h.snapLen = snapLen;
-  h.minorVersion = minor;
-  h.majorVersion = major;
-  return h;
+  header.magicNumber = magic;
+  header.linkType = linkType;
+  header.snapLen = snapLen;
+  header.minorVersion = minor;
+  header.majorVersion = major;
+  return header;
 }
