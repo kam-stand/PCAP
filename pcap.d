e6a5c78e6c0af57caeed5e608b00d3d8c50251a3
kamstand <kamrul.hassan@stonybrook.edu>
2025-04-04 15:28:56 -0400

    created packet struct

--- pcap.d
+++ pcap.d
@@ -9,14 +9,14 @@ import std.algorithm;
 
 enum HEADER_LENGTH = 24; // header is 24 octets
 enum MAJOR_MINOR_LENGTH = 2;
+
 enum HEADER_CHUNKS // every 4 bytes represent a portion of the header
 {
   MAGIC = 0,
   MAJOR_MINOR = 1,
   SNAP_LEN = 4,
-  LINK_TYEP = 5
+  LINK_TYPE = 5
 }
-
 
 /****
 
@@ -74,23 +74,41 @@ struct FileHeader
 
 }
 
+struct PacketRecord
+{
+  uint32_t seconds;
+  union  // determined by the magic number 
+  {
+    uint32_t nanoSeconds;
+    uint32_t microSeconds;
+  }
+  uint32_t capturedPacketLenght; // size of the data length for packet record
+  uint32_t originalPacketLength;
+  ubyte *data;
+  PacketRecord *next;
+  
+}
+
 FileHeader getHeader(ref ubyte[] content, in ENDIAN e)
 {
-  FileHeader header;
   ubyte[] fileHeader = content[0 .. HEADER_LENGTH];
-  auto c = chunks(fileHeader, MAGIC_HEADER);
-  
-  uint32_t snapLen = read_u32(c[4], e);
-  ubyte [] major_minor = c[1];
-  writeln(major_minor.length);
-  uint16_t major = read_u16(major_minor[0 .. MAJOR_MINOR_LENGTH], e);
-  uint16_t minor = read_u16(major_minor[MAJOR_MINOR_LENGTH .. $], e);
-  uint32_t magic = read_u32(c[0], e);
-  uint16_t linkType = read_u16(c[5], e);
-  header.magicNumber = magic;
-  header.linkType = linkType;
-  header.snapLen = snapLen;
-  header.minorVersion = minor;
-  header.majorVersion = major;
+  auto fileChunks = chunks(cast(ubyte[]) fileHeader, MAGIC_HEADER);
+  uint32_t magic = read_u32(fileChunks[HEADER_CHUNKS.MAGIC], e);
+  uint16_t major = read_u16(fileChunks[HEADER_CHUNKS.MAJOR_MINOR][0 .. MAJOR_MINOR_LENGTH], e);
+  uint16_t minor = read_u16(fileChunks[HEADER_CHUNKS.MAJOR_MINOR][MAJOR_MINOR_LENGTH .. $], e);
+  uint32_t snapLen = read_u32(fileChunks[HEADER_CHUNKS.SNAP_LEN], e);
+  uint16_t linkType = read_u16(fileChunks[HEADER_CHUNKS.LINK_TYPE], e);
+  FileHeader header = {magic, major, minor, snapLen, linkType};
   return header;
+}
+
+
+PacketRecord getPackets(ref ubyte[] content, in ENDIAN e)
+{
+  ubyte [] packets = content[HEADER_LENGTH .. $];
+  
+
+  PacketRecord p;
+
+  return p;
 }
