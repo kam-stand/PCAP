713134295922b477630508e348c8b6aa33111685
kamstand <kamrul.hassan@stonybrook.edu>
2025-04-08 22:22:24 -0400

    got packet header and file header. so far no gc allocations

--- source/main.d
+++ source/main.d
@@ -1,7 +1,13 @@
 import std.stdio;
+import system;
+import headers;
 
 
-void main(string[] args)
+void main(string[] args) 
 {
-    writeln("Hello world");
+    File f = File(args[1], "rb");
+    ENDIAN e = determineEndian(f);
+    writeln(getFileHeader(f, e));
+    writeln(getPacketHeader(f, e));
+    f.close();
 }
--- source/system.d
+++ source/system.d
@@ -32,25 +32,73 @@ enum ENDIAN : int
 
 enum MAGIC_HEADER_LENGTH = 4; // first 4 bytes are the magic numbers
 
-ENDIAN determineEndian(ref File f)
+ENDIAN determineEndian(File f)
 {
     ubyte[MAGIC_HEADER_LENGTH] magic;
         // interpret as little endian
     uint32_t magicLittle = 
-        (cast(uint)magic[0]) |
-        (cast(uint)magic[1] << 8) |
-        (cast(uint)magic[2] << 16) |
-        (cast(uint)magic[3] << 24);
+        (cast(uint32_t)magic[0]) |
+        (cast(uint32_t)magic[1] << 8) |
+        (cast(uint32_t)magic[2] << 16) |
+        (cast(uint32_t)magic[3] << 24);
 
     // interpret as big endian
     uint32_t magicBig = 
-        (cast(uint)magic[3]) |
-        (cast(uint)magic[2] << 8) |
-        (cast(uint)magic[1] << 16) |
-        (cast(uint)magic[0] << 24);
+        (cast(uint32_t)magic[3]) |
+        (cast(uint32_t)magic[2] << 8) |
+        (cast(uint32_t)magic[1] << 16) |
+        (cast(uint32_t)magic[0] << 24);
 
     if (magicLittle == MAGIC_NUMBER_LITTLE.V1 || MAGIC_NUMBER_LITTLE.V2) return ENDIAN.LITTLE;
     if (magicBig == MAGIC_NUMBER_BIG.V1 || MAGIC_NUMBER_BIG.V2) return ENDIAN.BIG;
 
     return ENDIAN.INVALID;
+}
+
+
+uint32_t convert_u32(ubyte[] content, ENDIAN e)
+{
+    uint32_t value;
+    switch(e)
+    {
+        case ENDIAN.LITTLE:
+            value = 
+            (cast(uint32_t)content[0]) |
+            (cast(uint32_t)content[1] << 8) |
+            (cast(uint32_t)content[2] << 16) |
+            (cast(uint32_t)content[3] << 24);
+            return value;
+        case ENDIAN.BIG:
+            value = 
+            (cast(uint32_t)content[3]) |
+            (cast(uint32_t)content[2] << 8) |
+            (cast(uint32_t)content[1] << 16) |
+            (cast(uint32_t)content[0] << 24);
+            return value;
+        default:
+            break;
+    }
+    return value;
+}
+
+
+uint16_t convert_u16(ubyte[] content, ENDIAN e)
+{
+    uint16_t value;
+    switch(e)
+    {
+        case ENDIAN.LITTLE:
+            value = 
+            (cast(uint16_t)content[0]) |
+            (cast(uint16_t)content[1] << 8);
+            return value;
+        case ENDIAN.BIG:
+            value = 
+            (cast(uint16_t)content[1]) |
+            (cast(uint16_t)content[0] << 8);
+            return value;
+        default:
+            break;
+    }
+    return value;
 }
