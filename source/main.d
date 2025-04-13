9f51b143905ba9dc6adc074337f6123fef674dd7
kamstand <kamrul.hassan@stonybrook.edu>
2025-04-13 00:20:36 -0400

    was able to get all packet data and verify size and data

--- source/main.d
+++ source/main.d
@@ -10,10 +10,18 @@ void main(string[] args)
     ENDIAN e = determineEndian(f);
     writeln(e);
     FILE_HEADER fh = getFileHeader(f, e);
-    PACKET_HEADER ph = getPacketHeader(f, e);
-    PACKET_DATA pd = getPacketData(f, e, ph);
-    //writeln(pd);
-    ETHERNET_HEADER et = getEthernetHeader(pd, e);
+    writefln("Size of file header: %d", FILE_HEADER_LENGTH);
+    // PACKET_HEADER ph = getPacketHeader(f, e);
+    // PACKET_DATA pd = getPacketData(f, e, ph);
+    // ETHERNET_HEADER et = getEthernetHeader(pd, e);
+
+    while (!(f.eof))
+    {
+        PACKET_HEADER ph = getPacketHeader(f, e);
+        writefln("Size of packet header: %d", PACKET_HEADER_LENGTH);
+        PACKET_DATA pd = getPacketData(f, e, ph);
+        writefln("Size of packet data %d: ", pd.data.length);
+    }
 
     f.close();
 }
