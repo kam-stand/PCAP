e163641909770db813c9910e85e85179130ef531
kamstand <kamrul.hassan@stonybrook.edu>
2025-05-14 21:08:51 -0400

    now have ethernet from last commits

--- source/ethernet.d
+++ source/ethernet.d
@@ -1,9 +1,8 @@
 module ethernet;
 import headers;
 import system;
-import std.stdio;
-import std.stdint;
-import std.algorithm;
+import core.stdc.stdio;
+import core.stdc.stdint;
 
 enum LINK_TYPE : uint16_t
 {
@@ -29,7 +28,7 @@ struct ETHERNET_HEADER
   ubyte[2] length_type;
 }
 
-ETHERNET_HEADER getEthernetHeader(PACKET_DATA *pd, ENDIAN e) @nogc
+ETHERNET_HEADER get_ethernet_header(PACKET_DATA *pd) @nogc
 {
   
   ETHERNET_HEADER eh;
@@ -52,5 +51,53 @@ enum DATAGRAM_TYPE
   IPV4 = 0x0800,
   ARP =  0x0806,
   IPX =  0x8137,
-  IPV6 = 0x86dd
+  IPV6 = 0x86dd,
+  INVALID
+}
+
+
+ETHERNET_FRAME determine_ethernet_frame(ETHERNET_HEADER ethernet_header)
+{
+
+ ushort ether_type = (cast(ushort)ethernet_header.length_type[0] << 8) |
+                        ethernet_header.length_type[1];
+  if (ether_type >= ETHERNET_FRAME.ETHERNET_II)
+  {
+    return ETHERNET_FRAME.ETHERNET_II;
+  }
+
+  else if (ether_type < ETHERNET_FRAME.IEEE_802_3)
+  {
+    return ETHERNET_FRAME.IEEE_802_3;
+  }
+
+  return ETHERNET_FRAME.INVALID;
+}
+
+DATAGRAM_TYPE determine_datagram_type(ETHERNET_HEADER header)
+{
+  ETHERNET_FRAME ethernet_frame = determine_ethernet_frame(header);
+  if (ethernet_frame == ETHERNET_FRAME.IEEE_802_3)
+  {
+    return DATAGRAM_TYPE.INVALID;
+  }
+
+    ushort ethertype = (cast(ushort)header.length_type[0] << 8) |
+                       header.length_type[1];
+
+    switch (ethertype)
+    {
+        case DATAGRAM_TYPE.IPV4:
+            return DATAGRAM_TYPE.IPV4;
+        case DATAGRAM_TYPE.ARP:
+            return DATAGRAM_TYPE.ARP;
+        case DATAGRAM_TYPE.IPX:
+            return DATAGRAM_TYPE.IPX;
+        case DATAGRAM_TYPE.IPV6:
+            return DATAGRAM_TYPE.IPV6;
+        default:
+            return DATAGRAM_TYPE.INVALID; 
+    }
+
+    return DATAGRAM_TYPE.INVALID;
 }
